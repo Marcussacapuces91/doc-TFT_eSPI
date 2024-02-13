6d39d5cb8bdbe22bd46694875f9a16eb2921bd1a
Marc Sibert <Marcussacapuces91@users.noreply.github.com>
2024-02-13 12:16:31 +0100

    Update TFT_eSPI.h

--- docs/source/TFT_eSPI.h
+++ docs/source/TFT_eSPI.h
@@ -1,1 +1,22 @@
+Header file TFT_eSPI.h
+======================
 
+**TFT_eSPI** (https://github.com/Bodmer/TFT_eSPI) est une librairie C qui, comme l'indique son GitHub, est 
+une bibliothèque compatible avec les IDE Arduino et PlatformIO, optimisée pour les Raspberry Pi Pico (RP2040),
+STM32, ESP8266 et ESP32 qui prend en charge les différents drivers d'écran.
+
+Check out the :doc:`usage` section for further information, including
+how to :ref:`installation` the project.
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+   datum
+   colors
+   class
